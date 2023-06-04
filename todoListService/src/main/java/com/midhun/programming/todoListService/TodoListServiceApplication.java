ab686047fea2e4cf60a62ceea1945d6a935d094a
midhunk07 <78677033+midhunk07@users.noreply.github.com>
2023-06-04 14:10:34 +0200

    Initial commit

---
+++ todoListService/src/main/java/com/midhun/programming/todoListService/TodoListServiceApplication.java
@@ -0,0 +1,13 @@
+package com.midhun.programming.todoListService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListServiceApplication.class, args);
+	}
+
+}
---
+++ todoListService/src/test/java/com/midhun/programming/todoListService/TodoListServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midhun.programming.todoListService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
